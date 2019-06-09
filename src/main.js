dc22af326a926c8a95a1cd7e8a1f222d87f4f63c
Yuri Rolandi <sd.rolandi@gmail.com>
2019-06-09 14:22:28 -0300

    Icones svg

--- src/main.js
+++ src/main.js
@@ -1,11 +1,18 @@
 import Vue from 'vue'
+import './plugins/fontawesome'
+import './plugins/fontawesome'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import 'font-awesome/css/font-awesome.css'
+
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons'   
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+import '@fortawesome/free-solid-svg-icons/index'
+import 'font-awesome/css/font-awesome.css'
+
+
 library.add(faUserSecret)
 
 Vue.component('font-awesome-icon', FontAwesomeIcon)
